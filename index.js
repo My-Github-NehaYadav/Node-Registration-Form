d6a99158b156e56ee29901b6fec36931edae65f6
Neha Yadav <nehayada948@gmail.com>
2022-05-28 14:52:46 +0530

    backend code of vue project

---
+++ index.js
@@ -0,0 +1,37 @@
+// const db = require('./connection/db_connection');
+// const routes = require('./routes/users.routes');
+// const fileUpload = require('express-fileupload');
+// const express = require('express');
+// const app = express();
+// app.use(express.json());
+// app.use(fileUpload());
+// app.use('/', routes);
+
+
+// app.listen(process.env.PORT, function (err) {
+//     if (err) {
+//         console.log(err);
+//     };
+//     console.log(`server is running on port no ${process.env.PORT}`);
+// });
+const cors = require ("cors");
+const db = require('./connection/db_connection');
+const routes = require('./routes/users.routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+console.log("backend runingggggggggggggggggggggggggggggggggg")
+
+app.listen(process.env.PORT, function (err) {
+    if (err) {
+        console.log(err);
+    };
+    console.log(`server is running on port no ${process.env.PORT}`);
+});
---
+++ routes/users.routes.js
@@ -0,0 +1,12 @@
+const userController = require('../controller/users.controller');
+const express = require('express');
+const route = express.Router();
+
+const uploadMulter = require('../middlewares/upload');
+const validation = require('../middlewares/validation');
+
+route.post('/signup', userController.sign_up);
+route.put('/uploadfile/:id', uploadMulter, validation, userController.uploadFile);
+route.post('/login', userController.login)
+
+module.exports = route;
